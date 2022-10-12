1149d0ae5a8a8bc12f1174cead1653eb5e2ba751
pranavmadhani <pranavmadhani25@gmail.com>
2022-10-12 14:47:11 -0500

    commit2

--- my-app-react/src/App.js
+++ my-app-react/src/App.js
@@ -22,4 +22,20 @@ function App() {
   );
 }
 
-export default App;
+function Goal(props) {
+  const isGoal = props.isGoal;
+  if (isGoal) {
+    return <MadeGoal/>;
+  }
+  return <MissedGoal/>;
+}
+function MissedGoal() {
+  return <h1>MISSED!</h1>;
+}
+
+function MadeGoal() {
+  return <h1>Goal!</h1>;
+}
+
+
+export {App,Goal,MissedGoal,MadeGoal} 
